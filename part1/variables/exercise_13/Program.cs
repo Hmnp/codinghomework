ca08f1193c0adc62b8fcb998497b0aaab52f6a74
Hmnp <hasan.mainali@centria.fi>
2020-01-24 14:53:55 +0200

    exercise 13 done

--- part1/variables/exercise_13/Program.cs
+++ part1/variables/exercise_13/Program.cs
@@ -6,6 +6,12 @@ namespace exercise_13
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Give a number!");
+      String userinput = Console.ReadLine();
+
+      double doublevalue = Convert.ToDouble(userinput);
+      Console.WriteLine("You gave " + doublevalue);
+
       // Write your code here:
 
     }
