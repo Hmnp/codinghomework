916954bda50779dc4363d39882a35eb8f61c3b57
Hmnp <hasan.mainali@centria.fi>
2020-01-24 10:40:15 +0200

    exercise 5 done

--- part1/printing/exercise_06/Program.cs
+++ part1/printing/exercise_06/Program.cs
@@ -7,6 +7,8 @@ namespace exercise_06
         public static void Main(string[] args)
         {
             Console.WriteLine("Give input!");
+            String message = "Hello";
+            Console.WriteLine(message);
             // Write your code here:
             
         }
