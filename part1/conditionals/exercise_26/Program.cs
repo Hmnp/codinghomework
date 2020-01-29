a2c131bbc7152dabb2193c4df59d454d35101b0d
Hmnp <hasan.mainali@centria.fi>
2020-01-29 15:17:33 +0200

    exercise 26 done

--- part1/conditionals/exercise_26/Program.cs
+++ part1/conditionals/exercise_26/Program.cs
@@ -6,6 +6,17 @@ namespace exercise_26
   {
     public static void Main(string[] args)
     {
+      int number = 1900;
+
+      Console.WriteLine("Give your age:");
+      string input_age = Console.ReadLine();
+      int input_value = Convert.ToInt32(input_age);
+
+      if (input_value < number)
+
+      {
+        Console.WriteLine("You're old");
+      }
       // Write your code here:
 
     }
