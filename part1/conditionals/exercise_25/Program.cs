47a513527f83faa19e6715f5d2f81e628e0e7ab7
Hmnp <hasan.mainali@centria.fi>
2020-01-29 15:11:19 +0200

    exercise 25 done

--- part1/conditionals/exercise_25/Program.cs
+++ part1/conditionals/exercise_25/Program.cs
@@ -6,6 +6,17 @@ namespace exercise_25
   {
     public static void Main(string[] args)
     {
+      int number = 1984;
+
+      Console.WriteLine("Give a number:");
+      string input_num = Console.ReadLine();
+      int input_value = Convert.ToInt32(input_num);
+
+      if (input_value == number)
+
+      {
+        Console.WriteLine("Orwell");
+      }
       // Write your code here:
 
     }
