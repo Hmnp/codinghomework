ffc8877a3d9e181ab3c9ca6ba37170e461882465
Hmnp <hasan.mainali@centria.fi>
2020-01-29 13:03:11 +0200

    exercise 19 done

--- part1/calculations/exercise_19/Program.cs
+++ part1/calculations/exercise_19/Program.cs
@@ -6,6 +6,18 @@ namespace exercise_19
   {
     public static void Main(string[] args)
     {
+       Console.WriteLine("Give the first number!");
+      string Inputnum_first = Console.ReadLine();
+      int First_num = Convert.ToInt32(Inputnum_first);
+
+      Console.WriteLine("Give the second number!");
+      string Inputnum_second = Console.ReadLine();
+      int Second_num = Convert.ToInt32(Inputnum_second);
+
+      
+
+      int sum = First_num + Second_num;
+      Console.WriteLine(First_num + " + " + Second_num + " = "  + sum);
 
       // Write your code here:
 
