d72aabbfe8a9e028dfc49cdc054160b1e74c2ab4
Hmnp <hasan.mainali@centria.fi>
2020-01-31 00:56:32 +0200

    exercise 30 done

--- part1/conditionals/exercise_30/Program.cs
+++ part1/conditionals/exercise_30/Program.cs
@@ -6,8 +6,47 @@ namespace exercise_30
   {
     public static void Main(string[] args)
     {
+       Console.WriteLine("Give your percent [0 - 100]:");
+      string input_percentage = Console.ReadLine();
+      int value_per = Convert.ToInt32(input_percentage);
+
+      if (value_per < 0)
+      {
+        Console.WriteLine("Impossible");
+      }
+      else if (value_per <= 49)
+      {
+        Console.WriteLine("Fail");
+      }
+      else if (value_per <= 59)
+      {
+        Console.WriteLine("Grade: 1");
+      }
+      else if (value_per <= 69)
+      {
+        Console.WriteLine("Grade: 2");
+      }
+      else if (value_per <= 79)
+      {
+        Console.WriteLine("Grade: 3");
+      }
+      else if (value_per <= 89)
+      {
+        Console.WriteLine("Grade: 4");
+      }
+      else if (value_per <= 100)
+      {
+        Console.WriteLine("Grade: 5");
+      }
+      else 
+      {
+        Console.WriteLine("Outstanding!");
+      }
+
+
       // Write your code here:
 
     }
   }
-}
+} 
+
