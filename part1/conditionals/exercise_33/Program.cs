2c9fdbf9f0c0877e087c9d2947f57d1d5f2f032d
Hmnp <hasan.mainali@centria.fi>
2020-01-31 01:40:56 +0200

    exercise 33 done

--- part1/conditionals/exercise_33/Program.cs
+++ part1/conditionals/exercise_33/Program.cs
@@ -6,6 +6,20 @@ namespace exercise_33
   {
     public static void Main(string[] args)
     {
+      Console.WriteLine("Give the first string:");
+      string first_str = Console.ReadLine();
+
+      Console.WriteLine("Give the second string:");
+      string Second_str = Console.ReadLine();
+
+      if (first_str == Second_str)
+      {
+        Console.WriteLine("Echo!");
+      }
+      else
+      {
+        Console.WriteLine("Nope!");
+      }
       // Write your code here:
 
     }
