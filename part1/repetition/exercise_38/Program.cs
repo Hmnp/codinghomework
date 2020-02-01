6752222f71f9deb4f80bb140a3e2ea9751670a67
Hmnp <hasan.mainali@centria.fi>
2020-02-01 02:43:53 +0200

    exercise 38 done

--- part1/repetition/exercise_38/Program.cs
+++ part1/repetition/exercise_38/Program.cs
@@ -6,6 +6,28 @@ namespace exercise_38
   {
     public static void Main(string[] args)
     {
+      int sum = 0;
+      
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        string input_num = Console.ReadLine();
+        int input_enter = Convert.ToInt32(input_num);
+
+        if (input_enter == 0)
+        {
+          break;
+        }
+        else if (input_enter < 0)
+        {
+          sum = sum + 1;
+        }
+        else 
+        {
+          continue;
+        }
+      }
+      Console.WriteLine("Total amount of negative numbers: " + sum);
       // Write your code here:
 
     }
