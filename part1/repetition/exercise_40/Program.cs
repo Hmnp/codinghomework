38fc3cf14291450f663647f77224dac2b4327952
Hmnp <hasan.mainali@centria.fi>
2020-02-01 03:15:49 +0200

    exercise 40 done

--- part1/repetition/exercise_40/Program.cs
+++ part1/repetition/exercise_40/Program.cs
@@ -6,6 +6,31 @@ namespace exercise_40
   {
     public static void Main(string[] args)
     {
+      int total = 0;
+      int sum = 0;
+      
+      while (true)
+      {
+        Console.WriteLine("Give a number:");
+        string input_num= Console.ReadLine();
+        int input_enter = Convert.ToInt32(input_num);
+
+        if (input_enter == 0)
+        {
+          break;
+        }
+        else
+        {
+          sum = sum + input_enter;
+          total = total + 1;
+        }
+        
+      }
+      {
+        Console.WriteLine("Total sum of numbers: " + sum);
+        Console.WriteLine("Total amount of numbers: " + total);
+        
+      }
 
       // Write your code here:
     }
