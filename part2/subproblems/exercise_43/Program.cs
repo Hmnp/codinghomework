fa3c36e97b6e0ab7d9a72a644f5620f3435e6cb2
Hmnp <hasan.mainali@centria.fi>
2020-02-01 18:43:19 +0200

    exercise 43 done

--- part2/subproblems/exercise_43/Program.cs
+++ part2/subproblems/exercise_43/Program.cs
@@ -6,6 +6,20 @@ namespace exercise_43
   {
     public static void Main(string[] args)
     {
+      string input_num1 = Console.ReadLine();
+      int enter_input1 = Convert.ToInt32(input_num1);
+
+     if (enter_input1 < 0)
+
+     {
+
+     Console.WriteLine(enter_input1 * (-1));  
+     }
+      else{
+
+      Console.WriteLine(enter_input1);  
+      }
+      
       // Write your code here:
       
     }
