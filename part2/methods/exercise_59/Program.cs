ff87589d8302b23cc98bf5082ea90b05e4dea432
Hmnp <hasan.mainali@centria.fi>
2020-02-03 00:00:31 +0200

    exercise 59 done

--- part2/methods/exercise_59/Program.cs
+++ part2/methods/exercise_59/Program.cs
@@ -6,11 +6,32 @@ namespace exercise_59
   {
     public static void Main(String[] args)
     {
-      int answer = Greatest(2, 7, 3);
+      int answer = Greatest(6, 9, 21);
       Console.WriteLine("Greatest: " + answer);
     }
 
     // Write your method here:
+    public static int Greatest(int first, int second, int third)
+    {
+      if (first > second && first > third)
+      {
+        return first;
+
+      }
+      
+      else if (second > first && second > third)
+      {
+        return second;
+
+    }
+    else 
+    {
+      return third;
+    }
+
+    }
+
+  // Write your method here:
 
   }
 }
