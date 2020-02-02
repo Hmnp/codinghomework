09b69b800289bf6b31f1ed2c08719d2efe240f21
Hmnp <hasan.mainali@centria.fi>
2020-02-02 21:32:56 +0200

    exercise 50 done

--- part2/methods/exercise_50/Program.cs
+++ part2/methods/exercise_50/Program.cs
@@ -6,13 +6,22 @@ namespace exercise_50
   {
     public static void Main(string[] args)
     {
-      // Call your method here:
- 
+      Console.WriteLine("How many times?");
+      int given_num = Convert.ToInt32(Console.ReadLine());
+      int i = 1;
+
+      while (i <= given_num)
+      {
+        PrintPhrase();
+        i++;
+      }
+
+    }
+    public static void PrintPhrase()
+    {
+      Console.WriteLine("In a hole in the ground there lived a method");
     }
 
-    // Write your method here:
-    public static void PrintPhrase() {
-
-    }
+     
   }
 }
