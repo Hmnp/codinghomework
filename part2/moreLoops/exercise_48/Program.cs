f0f4d92afa4e48aa1fc6441fcb1f60bdfc9bdbea
Hmnp <hasan.mainali@centria.fi>
2020-02-02 16:00:22 +0200

    exercise_48 done

--- part2/moreLoops/exercise_48/Program.cs
+++ part2/moreLoops/exercise_48/Program.cs
@@ -6,7 +6,49 @@ namespace exercise_48
   {
     public static void Main(string[] args)
     {
+       Console.WriteLine("Give numbers:"); 
+      
+      int sum = 0;
+      int amount_total = 0;
+      int odd_num = 0;
+      int even_num = 0;
 
+      
+      
+      while (true)
+      {
+        int number = Convert.ToInt32(Console.ReadLine());
+        
+        if (number != -1)
+        {
+          sum = sum + number;
+          amount_total++;
+          
+          if (number % 2 == 0)
+          {
+            even_num++;
+
+          }
+          else
+          {
+            odd_num++;
+
+          }
+
+        }
+        
+        else if (number == -1)
+        {
+          Console.WriteLine("Thx! Bye!");
+          Console.WriteLine("Sum: " + sum);
+          Console.WriteLine("Numbers: " + amount_total);
+          Console.WriteLine("Average: " + ((double)sum / amount_total));
+          Console.WriteLine("Even: " + even_num);
+          Console.WriteLine("Odd: " + odd_num);
+          break;
+        }
+
+      }
       // Write your code here:
  
     }
