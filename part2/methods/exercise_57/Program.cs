ef241526f9ac10f794dff18ca99b3389026d3a69
Hmnp <hasan.mainali@centria.fi>
2020-02-02 23:32:00 +0200

    exercise 57 done

--- part2/methods/exercise_57/Program.cs
+++ part2/methods/exercise_57/Program.cs
@@ -6,7 +6,8 @@ namespace exercise_57
   {
     public static int Sum(int number1, int number2, int number3, int number4)
     {
-      // write your code here
+      
+      return number1 + number2 + number3 + number4;// write your code here
       // remember to include return (at the end)!
     }
 
