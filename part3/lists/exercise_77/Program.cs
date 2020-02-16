5a6e6eafa0c3f6fdcc36cfc0e4f84e38f0a7d88f
Hmnp <hasan.mainali@centria.fi>
2020-02-16 12:13:18 +0200

    exercise_77 done

--- part3/lists/exercise_77/Program.cs
+++ part3/lists/exercise_77/Program.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 
 namespace exercise_77
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // You can try your method here if you want
-    }
+        public static void Main(string[] args)
+        {
+            List<string> strings = new List<string>();
 
-  }
+            strings.Add("First");
+            strings.Add("Second");
+            strings.Add("Third");
+
+            strings.ForEach(Console.WriteLine);
+
+            RemoveLast(strings);
+            strings.ForEach(Console.WriteLine);
+            // You can try your method here if you want
+        }
+        public static void RemoveLast(List<string> strings)
+        {
+          strings.RemoveAt(strings.Count - 1);
+        }
+
+    }
 }
 
 
