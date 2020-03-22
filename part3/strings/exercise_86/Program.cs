6f441d7926b6c21569761886fe3e54cc8b08d32e
Hmnp <hasan.mainali@centria.fi>
2020-03-23 00:00:02 +0200

    exercise 86 done

--- part3/strings/exercise_86/Program.cs
+++ part3/strings/exercise_86/Program.cs
@@ -7,7 +7,20 @@ namespace exercise_86
   {
     public static void Main(string[] args)
     {
+       while (true)
+      {
+        string input = Console.ReadLine();
+        if (input == "")
+        {
+          break;
+        }
+        string[] parted = input.Split(" ");
 
+        for (int i = 0; i < parted.Length; i++)
+        {
+          Console.WriteLine(parted[i]);
+        }
+      }
 
     }
   }
