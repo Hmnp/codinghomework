27f49a59286ac93691bd58862793a64f05e72d3a
Hmnp <hasan.mainali@centria.fi>
2020-04-13 22:48:43 +0300

    exercise 91 done

--- part3/strings/exercise_91/Program.cs
+++ part3/strings/exercise_91/Program.cs
@@ -7,6 +7,26 @@ namespace exercise_91
   {
     public static void Main(string[] args)
     {
+      string name = "";
+      int oldest = 0; 
+      while (true) 
+      {
+        string input = Console.ReadLine(); 
+        if (input == "") 
+        {
+          break;
+        }
+        string[] parts = input.Split(","); 
+        int age = Convert.ToInt32(parts[1]);
+        
+        if (age > oldest)
+        {
+          oldest = age;
+          name = parts[0];
+        }
+      }
+      Console.WriteLine("Name of the oldest: " + name); 
+
 
     }
   }
