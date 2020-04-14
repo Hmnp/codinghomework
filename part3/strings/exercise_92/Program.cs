a7be819509bf920c92ba15a016234cfe3f8250b8
Hmnp <hasan.mainali@centria.fi>
2020-04-15 00:46:25 +0300

    exercise 92 done

--- part3/strings/exercise_92/Program.cs
+++ part3/strings/exercise_92/Program.cs
@@ -7,6 +7,31 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
+      string name = "";
+      int longest = 0;
+      int highest = 0;
+      while (true) 
+      {
+        string input = Console.ReadLine(); 
+        if (input == "") 
+        {
+          break;
+        }
+        string[] parts = input.Split(","); 
+        int length = input.Length;
+        int age = 2020 - Convert.ToInt32(parts[1]);
+        if (length > longest) 
+        {
+          longest = length;
+          name = parts[0];
+        }
+        if (age > highest) 
+        {
+          highest = age;
+        }
+      }
+      Console.WriteLine("Longest name: " + name); 
+      Console.WriteLine("Highest age: " + highest);   
 
     }
   }
