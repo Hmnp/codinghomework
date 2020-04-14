8c47edad2a770b4e91d6523a1f25cbaa58910b40
Hmnp <hasan.mainali@centria.fi>
2020-04-15 01:33:16 +0300

    exercise 94 done

--- part4/objects/exercise_94/Program.cs
+++ part4/objects/exercise_94/Program.cs
@@ -6,7 +6,16 @@ namespace exercise_94
   {
     public static void Main(string[] args)
     {
-      // DO NOT TOUCH THE OTHER FILE!
+      Account HeikkisAccount = new Account ("Heikki's account", 1000.00);
+      Account personalAccount = new Account ("Personal account", 0);
+
+      HeikkisAccount.Withdrawal(100.00);
+      personalAccount.Deposit(100.00);
+
+      Console.WriteLine(HeikkisAccount);
+      Console.WriteLine(personalAccount);
+
+           // DO NOT TOUCH THE OTHER FILE!
       // Do your code here!
 
     }
